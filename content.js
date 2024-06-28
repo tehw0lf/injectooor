df19e39a91a11d407b33bd1b8bc9dbd25a89085e
tehw0lf <tehwolf@protonmail.com>
2024-06-29 00:38:53 +0200

    Ensure script is loaded

--- content.js
+++ content.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const injectScript = () => {
-    const existingScript = document.getElementById("injectedScript");
-    if (existingScript) existingScript.remove();
-
-    const scriptElement = document.createElement("script");
-    scriptElement.id = "injectedScript";
-    scriptElement.textContent = script;
-    document.body.appendChild(scriptElement);
-  };
-
   browser.storage.local.get(window.location.href).then((result) => {
     const script = result[window.location.href];
     if (script) {
-      injectScript(script);
+      const existingScript = document.getElementById("injectedScript");
+      if (existingScript) existingScript.remove();
+
+      const scriptElement = document.createElement("script");
+      scriptElement.id = "injectedScript";
+      scriptElement.textContent = script;
+      document.body.appendChild(scriptElement);
     }
   });
 });
